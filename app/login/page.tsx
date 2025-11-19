c540914781497c470a30834e368c7dddd86c52f9
Kamil Wachnicki <kamilwachnicki321@gmail.com>
2025-11-19 19:56:24 +0100

    a

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import PageLayout from "@/components/PageLayout";
+import Login from "@/components/Login";
+
+export default function Home() {
+    return (
+        <PageLayout>
+            <Login />
+        </PageLayout>
+    );
+}
